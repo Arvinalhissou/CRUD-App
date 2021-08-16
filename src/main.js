29bf5085e4c46148468ae645550f400a8710d5a4
Arvin Alhissou <arvin.alhissou@gmail.com>
2021-08-16 20:43:48 +0300

    install vue-router and render it in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
-createApp(App).mount('#app')
+const router = new VueRouter({
+  mode: "history",
+  base: __dirname,
+  routes: [
+    {
+      path: "/create",
+      name: "create",
+      component: () => import("./components/CreateDocument.vue"),
+    },
+    {
+      path: "/collection",
+      name: "collection",
+      component: () => import("./components/DeleteDocument.vue"),
+    },
+    {
+      path: "/update",
+      name: "update",
+      component: () => import("./components/UpdateDocument.vue"),
+    },
+  ],
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
+
+//createApp(App).mount("#app");
